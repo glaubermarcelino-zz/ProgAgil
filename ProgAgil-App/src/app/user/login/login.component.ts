20f846a9c8ae4b78bbe73ab184cecc0154cea94d
glaubermarcelino <gmsoft.informatica@gmail.com>
2019-09-30 17:59:02 -0300

    Adicoinado FormGroup e validators

--- ProgAgil-App/src/app/user/login/login.component.ts
+++ ProgAgil-App/src/app/user/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,9 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  titulo: string;
+  titulo = 'Login';
   model: any = {};
-  constructor() { }
+  constructor(public router: Router) { }
 
   ngOnInit() {
   }
--- ProgAgil-App/src/app/user/registration/registration.component.ts
+++ ProgAgil-App/src/app/user/registration/registration.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-registration',
@@ -6,10 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent implements OnInit {
-
-  constructor() { }
-
+  registerForm: FormGroup;
+  
+  constructor(public fb: FormBuilder, private toastService: ToastrService) {}
   ngOnInit() {
+    this.validation();
   }
-
+  validation() {
+    this.registerForm = this.fb.group({
+      fullName: ['', Validators.required],
+      email   : ['', [Validators.required, Validators.email]],
+      userName: ['', Validators.required],
+      passwords: this.fb.group({
+        password: ['', [Validators.required, Validators.minLength(4)]],
+        confirmPassword: ['', [Validators.required, Validators.minLength(4)]]
+      }, {validator : this.compararSenhas})
+    });
+  }
+  cadastrarusuario() {
+    console.log('usuario cadastrado');
+  }
+  compararSenhas(fb: FormGroup) {
+    const confirmSenhaCtrl = fb.get('confirmPassword');
+    if (confirmSenhaCtrl.errors == null || 'mismatch' in confirmSenhaCtrl.errors) {
+      if (fb.get('password').value !== confirmSenhaCtrl.value ) {
+        fb.get('password').setErrors({mismatch: true});
+      } else {confirmSenhaCtrl.setErrors(null); }
+    }
+  }
 }
